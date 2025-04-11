6a751665d47e04186ef61773f212ab3ba01db92c
lucidrains <lucidrains@gmail.com>
2025-04-11 09:22:03 -0700

    complete evolutionary algorithm

--- evolutionary_policy_optimization/epo.py
+++ evolutionary_policy_optimization/epo.py
@@ -6,6 +6,8 @@ from torch import nn, cat
 import torch.nn.functional as F
 from torch.nn import Linear, Module, ModuleList
 
+from einops import rearrange, repeat
+
 # helpers
 
 def exists(v):
@@ -13,6 +15,9 @@ def exists(v):
 
 def default(v, d):
     return v if exists(v) else d
+
+def identity(t):
+    return t
 
 def xnor(x, y):
     return not (x ^ y)
@@ -145,6 +150,99 @@ class MLP(Module):
 
 class EPO(Module):
     def __init__(
-        self
+        self,
+        num_latents,                   # same as gene pool size
+        dim_latent,                    # gene dimension
+        crossover_random = True,       # random interp from parent1 to parent2 for crossover, set to `False` for averaging (0.5 constant value)
+        l2norm_latent = False,         # whether to enforce latents on hypersphere,
+        frac_tournaments = 0.25,       # fraction of genes to participate in tournament - the lower the value, the more chance a less fit gene could be selected
+        frac_natural_selection = 0.25, # number of least fit genes to remove from the pool
+        frac_elitism = 0.1,            # frac of population to preserve from being noised
+        mutation_strength = 1.         # factor to multiply to gaussian noise as mutation to latents
     ):
         super().__init__()
+        maybe_l2norm = l2norm if l2norm_latent else identity
+
+        latents = torch.randn(num_latents, dim_latent)
+
+        if l2norm_latent:
+            latents = maybe_l2norm(latents, dim = -1)
+
+        self.latents = nn.Parameter(latents, requires_grad = False)
+
+        self.maybe_l2norm = maybe_l2norm
+
+        # some derived values
+
+        assert 0. < frac_tournaments < 1.
+        assert 0. < frac_natural_selection < 1.
+        assert 0. <= frac_elitism < 1.
+        assert (frac_natural_selection + frac_elitism) < 1.
+
+        self.dim_latent = dim_latent
+        self.num_natural_selected_out = int(frac_natural_selection * num_latents)
+
+        self.num_tournament_participants = int(frac_tournaments * num_latents)
+        self.crossover_random  = crossover_random
+
+        self.mutation_strength = mutation_strength
+        self.num_elites = int(frac_elitism * num_latents)
+
+    @torch.no_grad() # non-gradient optimization, at least, not on the genetic level
+    def genetic_algorithm_step(
+        self,
+        fitness # Float['p']
+    ):
+        """
+        p - population
+        g - gene dimension
+        """
+
+        genes = self.latents # the latents are the genes
+
+        assert genes.shape[0] == fitness.shape[0]
+
+        # 1. natural selection is simple in silico
+        # you sort the population by the fitness and slice off the least fit end
+
+        fitness, sorted_indices = fitness.sort()
+        sorted_genes = genes[sorted_indices]
+        genes = sorted_genes[self.num_natural_selected_out:]
+
+        # 2. for finding pairs of parents to replete gene pool, we will go with the popular tournament strategy
+
+        batch_randperm = torch.randn((self.num_natural_selected_out, self.num_tournament_participants)).argsort(dim = -1)
+
+        participants = genes[batch_randperm]
+        participant_fitness = fitness[batch_randperm]
+
+        tournament_winner_indices = participant_fitness.topk(2, dim = -1).indices
+
+        tournament_winner_indices = repeat(tournament_winner_indices, '... -> ... g', g = self.dim_latent)
+
+        parents = participants.gather(-2, tournament_winner_indices)
+
+        # 3. do a crossover of the parents - in their case they went for a simple averaging, but since we are doing tournament style and the same pair of parents may be re-selected, lets make it random interpolation
+
+        parent1, parent2 = parents.unbind(dim = 1)
+        children = crossover_latents(parent1, parent2, random = self.crossover_random)
+
+        # 4. append children to gene pool
+
+        genes = cat((children, genes))
+
+        # 5. they use the elitism strategy to protect best performing genes from being changed
+
+        genes, elitists = genes[:-self.num_elites], genes[-self.num_elites:]
+
+        # 6. mutate with gaussian noise - todo: add drawing the mutation rate from exponential distribution, from the fast genetic algorithms paper from 2017
+
+        genes = mutation(genes, mutation_strength = self.mutation_strength)
+
+        # 7. add back the elites
+
+        genes = cat((genes, elitists))
+
+        # store the genes for the next interaction with environment for new fitness values (a function of reward and other to be researched measures)
+
+        self.latents.copy_(self.maybe_l2norm(genes))
