a3f3983c90076031521a60e35608609d76e0cfd9
Phil Wang <lucidrains@gmail.com>
2025-04-12 05:41:52 -0700

    keep the implementation faithful and move all excursions to a separate file

--- evolutionary_policy_optimization/epo.py
+++ evolutionary_policy_optimization/epo.py
@@ -27,32 +27,6 @@ def l2norm(t):
 
 # evolution related functions
 
-def crossover_weights(w1, w2, transpose = False):
-    assert w2.shape == w2.shape
-    assert w1.ndim == 2
-
-    if transpose:
-        w1, w2 = w1.t(), w2.t()
-
-    rank = min(w2.shape)
-    assert rank >= 2
-
-    u1, s1, v1 = torch.svd(w1)
-    u2, s2, v2 = torch.svd(w2)
-
-    mask = torch.randperm(rank) < (rank // 2)
-
-    u = torch.where(mask[None, :], u1, u2)
-    s = torch.where(mask, s1, s2)
-    v = torch.where(mask[None, :], v1, v2)
-
-    out = u @ torch.diag_embed(s) @ v.mT
-
-    if transpose:
-        out = out.t()
-
-    return out
-
 def crossover_latents(
     parent1, parent2,
     weight = None,
